404f3a4704bddc170306f4bb6dde35a1b86ec3a9
Eduardo Mejia <mejiaeduardo@yahoo.com>
2024-10-13 00:10:26 -0400

    moved javascript to separate file

--- sign-up.js
+++ sign-up.js
@@ -1,3 +1,19 @@
 // Confirm passwords are the same
-document.querySelector("#password").value ===
-  document.querySelector("#confirm-password").value;
+let passwordError = document.querySelector("#password-error");
+const button = document.querySelector("#btn");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+
+button.addEventListener("click", (e) => {
+  passwordError.style.color = "red";
+  passwordError.innerHTML = "*Passwords do not match";
+  console.log(e);
+});
+if (password == "" && confirmPassword == "") {
+  passwordError.innerHTML = "*Passwords do not match!";
+}
+
+if (password !== confirmPassword) {
+  console.log("Passwords don't match!");
+  // passwordError.innerText = "Passwords do not match!";
+}
