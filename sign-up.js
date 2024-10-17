88913dcaf1d178c77a42cc4b9ec1aa36cfbdbd92
Eduardo Mejia <mejiaeduardo@yahoo.com>
2024-10-17 19:04:43 -0400

    fixed passwords testing

--- sign-up.js
+++ sign-up.js
@@ -6,17 +6,21 @@ const confirmPassword = document.querySelector("#confirm-password");
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  if (password.value == "" && confirmPassword.value == "") {
+
+  if (password.value !== confirmPassword.value) {
+    console.log(password, confirmPassword);
     passwordError.style.color = "red";
     passwordError.innerHTML = "*Passwords do not match!";
+    password.value = "";
+    confirmPassword.value = "";
+
+    // passwordError.innerText = "Passwords do not match!";
+  } else {
+    passwordError.style.color = "green";
+    passwordError.innerHTML = "*Good to go.";
+    password.value = "";
+    confirmPassword.value = "";
   }
 
   console.log(e);
 });
-
-if (password !== confirmPassword) {
-  passwordError.style.color = "red";
-  passwordError.innerHTML = "*Passwords do not match!";
-
-  // passwordError.innerText = "Passwords do not match!";
-}
