bdedfd00657fd66ca60fd0617d10e6c8761b045b
Eduardo Mejia <mejiaeduardo@yahoo.com>
2024-11-18 16:23:04 -0500

    modified inputs slightly

--- sign-up.js
+++ sign-up.js
@@ -1,26 +1,18 @@
 // Confirm passwords are the same
 let passwordError = document.querySelector("#password-error");
-const button = document.querySelector("#btn");
+const form = document.querySelector("#form");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-
+form.addEventListener("submit", (e) => {
   if (password.value !== confirmPassword.value) {
-    console.log(password, confirmPassword);
     passwordError.style.color = "red";
     passwordError.innerHTML = "*Passwords do not match!";
     password.value = "";
     confirmPassword.value = "";
-
-    // passwordError.innerText = "Passwords do not match!";
-  } else {
-    passwordError.style.color = "green";
-    passwordError.innerHTML = "*Good to go.";
-    password.value = "";
-    confirmPassword.value = "";
+    e.preventDefault();
   }
-
-  console.log(e);
 });
+
+password.style.color = "green";
+confirmPassword.style.color = "green";
